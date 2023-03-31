36754d0f74348a855f030f9f2494dc711f24c32d
GioF_71 <giovanni.fulco@gmail.com>
2023-03-31 12:56:09 +0200

    AUDIO_GID implemented

--- app/bin/run-xfce.sh
+++ app/bin/run-xfce.sh
@@ -39,12 +39,29 @@ else
     echo "user $USER_NAME already exists."
 fi
 
+if [ -n ${AUDIO_GID} ]; then
+    if [ $(getent group $AUDIO_GID) ]; then
+        echo "  Group with gid $AUDIO_GID already exists"
+    else
+        echo "  Creating group with gid $AUDIO_GID"
+        groupadd -g $AUDIO_GID vnc_user_audio
+    fi
+    echo "  Adding $USER_NAME to gid $AUDIO_GID"
+    AUDIO_GRP=$(getent group $AUDIO_GID | cut -d: -f1)
+    echo "  gid $AUDIO_GID -> group $AUDIO_GRP"
+    if id -nG "$USER_NAME" | grep -qw "$AUDIO_GRP"; then
+        echo "  User $USER_NAME already belongs to group audio (GID ${AUDIO_GID})"
+    else
+        usermod -a -G $AUDIO_GRP $USER_NAME
+        echo "  Successfully added $USER_NAME to group audio (GID ${AUDIO_GID})"
+    fi
+fi
+
 cd $HOME_DIR
 
 if [ ! -f "$HOME_DIR/.vnc/xstartup" ]; then
     echo "Creating xstartup file..."
     mkdir $HOME_DIR/.vnc
-    cp /app/assets/xstartup $HOME_DIR/.vnc/
     chown -R $USER_NAME:$GROUP_NAME $HOME_DIR/.vnc
     chmod 700 $HOME_DIR/.vnc
     chmod 755 $HOME_DIR/.vnc/xstartup
